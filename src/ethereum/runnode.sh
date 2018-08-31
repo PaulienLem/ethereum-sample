f043db8500b39e2e6e9a01e79377eec848fec97d
paulienlem <paulien.lemay@sweetmustard.be>
2018-08-31 15:23:24 +0200

    simplify scripts

--- src/ethereum/runnode.sh
+++ src/ethereum/runnode.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
 IMGNAME="ethereum/client-go:v1.8.12"
-NODE_NAME=$1
-NODE_NAME=${NODE_NAME:-"node1"}
-DETACH_FLAG=${DETACH_FLAG:-"-d"}
-CONTAINER_NAME="ethereum-$NODE_NAME"
-DATA_ROOT=${DATA_ROOT:-"$(pwd)/.ether-$NODE_NAME"}
+DATA_ROOT=${DATA_ROOT:-"$(pwd)/.ether-miner1"}
 DATA_HASH=${DATA_HASH:-"$(pwd)/.ethash"}
-docker stop $CONTAINER_NAME
-docker rm $CONTAINER_NAME
-RPC_PORTMAP=
-RPC_ARG=
-if [[ ! -z $RPC_PORT ]]; then
-    RPC_ARG='--rpc --rpcaddr=0.0.0.0 --rpcport 8545 --rpcapi=db,eth,net,web3,personal --rpccorsdomain "*"'
-    RPC_PORTMAP="-p $RPC_PORT:8545"
-fi
 
 ENODE_LINE=$(docker logs ethereum-bootnode 2>&1 | grep enode | head -n 1)
 MYIP=$(docker exec ethereum-bootnode ifconfig eth0 | awk '/inet addr/{print substr($2,6)}')
@@ -21,25 +9,21 @@ ENODE_LINE=$(echo $ENODE_LINE | sed "s/127\.0\.0\.1/$MYIP/g" | sed "s/\[\:\:\]/$
 BOOTNODE_URL= "enode:${ENODE_LINE#*enode:}"
 
 if [ ! -f $(pwd)/genesis.json ]; then
-    GEN_NONCE="0xeddeadbabeeddead"
-    GEN_CHAIN_ID=1981
     GEN_ALLOC='"0x0000000000000000000000000000000000000001": {"balance": "100000"}, "0xf6165e3ab53a51e974af53b4af28e53f6dc405e2": {"balance": "100000"}'
-    sed "s/\${GEN_NONCE}/$GEN_NONCE/g" src/genesis.json.template | sed "s/\${GEN_ALLOC}/$GEN_ALLOC/g" | sed "s/\${GEN_CHAIN_ID}/$GEN_CHAIN_ID/g" > genesis.json    exit
+    sed "s/\0xeddeadbabeeddead/0xeddeadbabeeddead/g" src/genesis.json.template | sed "s/\${GEN_ALLOC}/$GEN_ALLOC/g" | sed "s/\1981/1981/g" > genesis.json    exit
 fi
+
 if [ ! -d $DATA_ROOT/keystore ]; then
-    echo "$DATA_ROOT/keystore not found, running 'geth init'..."
     docker run --rm \
         -v $DATA_ROOT:/root/.ethereum \
         -v $(pwd)/genesis.json:/opt/genesis.json \
         $IMGNAME init /opt/genesis.json
-    echo "...done!"
 fi
-echo "Running new container $CONTAINER_NAME..."
-docker run $DETACH_FLAG --name $CONTAINER_NAME \
+
+docker run -d --name ethereum-miner1 \
     --network ethereum \
     -p 8545:8545 \
     -v $DATA_ROOT:/root/.ethereum \
     -v $DATA_HASH:/root/.ethash \
     -v $(pwd)/genesis.json:/opt/genesis.json \
-    $RPC_PORTMAP \
-    $IMGNAME --bootnodes=$BOOTNODE_URL --rpc --rpcaddr=0.0.0.0 --rpcport 8545 --rpcapi=db,eth,net,web3,personal --rpccorsdomain "*" --cache=512 --verbosity=4 --maxpeers=3 ${@:2}
+    $IMGNAME --bootnodes=$BOOTNODE_URL --rpc --rpcaddr=0.0.0.0 --rpcport 8545 --rpcapi=db,eth,net,web3,personal --rpccorsdomain "*" --cache=512 --verbosity=4 --maxpeers=3  --mine --minerthreads=1 --etherbase="0x0000000000000000000000000000000000000001"
