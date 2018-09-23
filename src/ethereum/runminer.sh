54835f0a866e7f59c05d50ec0e309620fc7e9486
paulienlem <paulien.lemay@sweetmustard.be>
2018-09-23 11:30:58 +0200

    more fixed variables

--- src/ethereum/runminer.sh
+++ src/ethereum/runminer.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 docker stop ethereum-bootnode
 docker rm ethereum-bootnode
-IMGNAME="ethereum/client-go:alltools-v1.8.12"
 DATA_ROOT=${DATA_ROOT:-$(pwd)}
 mkdir -p $DATA_ROOT/.bootnode
 
 if [ ! -f $DATA_ROOT/.bootnode/boot.key ]; then
-    docker run --rm -v $DATA_ROOT/.bootnode:/opt/bootnode $IMGNAME bootnode --genkey /opt/bootnode/boot.key
+    docker run --rm -v $DATA_ROOT/.bootnode:/opt/bootnode "ethereum/client-go:alltools-v1.8.12" bootnode --genkey /opt/bootnode/boot.key
 fi
 
 [ ! "$(docker network ls | grep ethereum)" ] && docker network create ethereum
 [[ -z $BOOTNODE_SERVICE ]] && BOOTNODE_SERVICE="127.0.0.1"
 
 docker run -d --name ethereum-bootnode \
-    -v $DATA_ROOT/.bootnode:/opt/bootnode --network ethereum $IMGNAME bootnode --nodekey /opt/bootnode/boot.key --verbosity=3 "$@"
+    -v $DATA_ROOT/.bootnode:/opt/bootnode --network ethereum "ethereum/client-go:alltools-v1.8.12" bootnode --nodekey /opt/bootnode/boot.key --verbosity=3 "$@"
 
-NODE_NAME="miner1"
 ETHERBASE=${ETHERBASE:-"0x0000000000000000000000000000000000000001"}
-./runnode.sh $NODE_NAME --mine --minerthreads=1 --etherbase="$ETHERBASE"
+./runnode.sh "miner1" --mine --minerthreads=1 --etherbase="$ETHERBASE"
--- src/ethereum/runnode.sh
+++ src/ethereum/runnode.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
-NODE_NAME=$1
-NODE_NAME=${NODE_NAME:-"node1"}
-CONTAINER_NAME="ethereum-$NODE_NAME"
-DATA_ROOT=${DATA_ROOT:-"$(pwd)/.ether-$NODE_NAME"}
+DATA_ROOT=${DATA_ROOT:-"$(pwd)/.ether-miner1"}
 DATA_HASH=${DATA_HASH:-"$(pwd)/.ethash"}
-docker stop $CONTAINER_NAME
-docker rm $CONTAINER_NAME
-RPC_ARG='--rpc --rpcaddr=0.0.0.0 --rpcport 8545 --rpcapi=db,eth,net,web3,personal --rpccorsdomain "*"'
 RPC_PORTMAP="-p $RPC_PORT:8545"
 
 ENODE_LINE=$(docker logs ethereum-bootnode 2>&1 | grep enode | head -n 1)
@@ -18,11 +12,7 @@ if [ ! -d $DATA_ROOT/keystore ]; then
     docker run --rm -v $DATA_ROOT:/root/.ethereum -v $(pwd)/genesis.json:/opt/genesis.json "ethereum/client-go:v1.8.12" init /opt/genesis.json
 fi
 
-docker run -d --name $CONTAINER_NAME \
-    --network ethereum \
-    -p 8545:8545 \
-    -v $DATA_ROOT:/root/.ethereum \
-    -v $DATA_HASH:/root/.ethash \
-    -v $(pwd)/genesis.json:/opt/genesis.json \
-    $RPC_PORTMAP \
+docker run -d --name "ethereum-miner1" \
+    --network ethereum -p 8545:8545 -v $DATA_ROOT:/root/.ethereum -v $DATA_HASH:/root/.ethash \
+    -v $(pwd)/genesis.json:/opt/genesis.json $RPC_PORTMAP \
     "ethereum/client-go:v1.8.12" --bootnodes=$BOOTNODE_URL --rpc --rpcaddr=0.0.0.0 --rpcport 8545 --rpcapi=db,eth,net,web3,personal --rpccorsdomain "*" --cache=512 --verbosity=4 --maxpeers=3 ${@:2}
